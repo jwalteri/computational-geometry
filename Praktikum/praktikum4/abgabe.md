c6d5509addd849ff307ef77fffe76d398002a3c7
Johannes Walter <jwalter@hm.edu>
2024-07-02 02:53:39 +0200

    graphen generiert

---
+++ Praktikum/praktikum4/abgabe.md
@@ -0,0 +1,20 @@
+# Praktikum 4
+
+## Aufgabenstellung
+Installieren Sie das Programm qhull, erzeugen Sie zufällige Punktemengen und berechnen Sie mit qhull konvexe Hüllen, auch in höheren Dimensionen (qhull bringt ein Werkzeug zur Erzeugung von Punktmengen mit). Plotten Sie die Zeiten für zunehmende Punktanzahlen bei unterschiedlichen Dimensionen (2-8). Versuchen Sie, die Ausgaben von qhull bei "geschwätzigster" Einstellung nachzuvollziehen, zu verstehen und ggf. mit Inhalten dieser Lehrveranstaltung in Einklang zu bringen.
+
+## Verwendete Software
+- [Qhull](http://www.qhull.org/)
+- [rbox](http://www.qhull.org/html/rbox.htm)
+- [qconvex](http://www.qhull.org/html/qconvex.htm)
+
+## Generierte Punkte
+Qhull liefert mit rBox die Möglichkeit mit verschiedenen Optionen Punkte generieren zu lassen. Es wurden zufällige Punkte in folgenden Formen:
+
+1. 3D Würfel
+2. Reguläre 3-D-Spirale
+3. Reguläres Polygon
+4. 2-D-Sphäre
+
+## Zufällige Punkte in einem Würfel
+
--- Praktikum/praktikum4/graph.py
+++ Praktikum/praktikum4/graph.py
@@ -2,7 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Einlesen der CSV-Datei
-df = pd.read_csv('rbox.csv')
+df = pd.read_csv('csv/cospherical.csv')
+output1 = "graphs/cospherical-pdt.png"
+output2 = "graphs/cospherical-pcs.png"
 
 # Extrahieren der Dimension und der Anzahl der Punkte aus dem Befehl
 df['Dimension'] = df['Befehl'].str.extract(r'rbox \d+ D(\d+)')
@@ -17,26 +19,30 @@ df['CPU Seconds'] = pd.to_numeric(df['CPU Seconds'])
 plt.figure(figsize=(10, 6))
 for dimension in df['Dimension'].unique():
     subset = df[df['Dimension'] == dimension]
-    plt.plot(subset['Anzahl Punkte'], subset['Distance Tests'], marker='o', label=f'Dimension {dimension}')
+    # plt.plot(subset['Anzahl Punkte'], subset['Distance Tests'], marker='o', label=f'Dimension {dimension}')
+    plt.plot(subset['Anzahl Punkte'], subset['Distance Tests'], marker='o', label=f'distance tests')
 plt.xlabel('Anzahl der Punkte')
 plt.ylabel('Anzahl der Distance Tests')
-plt.title('Anzahl der Distance Tests pro Dimension')
+plt.title('Verhältnis Distance Tests und Anzahl der Punkte')
 plt.legend()
 plt.xscale('log')
 plt.yscale('log')
 plt.grid(True, which="both", ls="--")
+plt.savefig(output1)  # Speichern des Graphen in eine Datei
 plt.show()
 
 # Graph 2: Pro Dimension eine Linie (Anzahl der Punkte vs. Anzahl der CPU_Seconds)
 plt.figure(figsize=(10, 6))
 for dimension in df['Dimension'].unique():
     subset = df[df['Dimension'] == dimension]
-    plt.plot(subset['Anzahl Punkte'], subset['CPU Seconds'], marker='o', label=f'Dimension {dimension}')
+    # plt.plot(subset['Anzahl Punkte'], subset['CPU Seconds'], marker='o', label=f'Dimension {dimension}')
+    plt.plot(subset['Anzahl Punkte'], subset['CPU Seconds'], marker='o', label=f'cpu seconds')
 plt.xlabel('Anzahl der Punkte')
 plt.ylabel('CPU Seconds')
-plt.title('CPU Seconds pro Dimension')
+plt.title('Verhältnis CPU Seconds und Anzahl der Punkte')
 plt.legend()
 plt.xscale('log')
 plt.yscale('log')
 plt.grid(True, which="both", ls="--")
+plt.savefig(output2)  # Speichern des Graphen in eine Datei
 plt.show()
